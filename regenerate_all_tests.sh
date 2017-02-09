60542a61589fc3da178fe6aac83ab55bd2490607
ArturoCampos <arturocampos82@gmail.com>
2017-02-09 13:02:58 +0100

    tests: regenerated lalr (minor cosmetic changes)

--- regenerate_all_tests.sh
+++ regenerate_all_tests.sh
@@ -1,11 +1,13 @@
 test_folder(){
     cd $1
     printf $1
+    printf " >>> "
     ./regenerate_tests.sh
     cd .. 
 }
 
-# test_folder LALR
+test_folder LALR
 test_folder LR1
+test_folder GrammarAnalysis
 test_folder RecDescent
 
