f26aec30b3d75fd66addf3edfe51a3f854b8ad82
ArturoCampos <arturocampos82@gmail.com>
2017-03-02 10:15:40 +0100

    Improving test's output

--- RecDescent/src/tests/test_code_generator.cpp
+++ RecDescent/src/tests/test_code_generator.cpp
@@ -48,9 +48,9 @@ void parse(const std::string& str, G& g)
     pm.Run();
   }
   if(unit.GetAstProg() != nullptr){
-    std::cout << "\nAST dump:\n";
-    ASTVisitorDump visitor_dump(unit, true);
-    visitor_dump.Visit(*unit.GetAstProg());
+//     std::cout << "\nAST dump:\n";
+//     ASTVisitorDump visitor_dump(unit, true);
+//     visitor_dump.Visit(*unit.GetAstProg());
 
 
     IRGenerator visitor_irgen(unit);
