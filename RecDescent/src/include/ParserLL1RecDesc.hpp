6db125c144df96e02e75df79911d573b75cc2902
ArturoCampos <arturocampos82@gmail.com>
2017-03-06 12:36:50 +0100

    WIP

--- RecDescent/src/include/ParserLL1RecDesc.hpp
+++ RecDescent/src/include/ParserLL1RecDesc.hpp
@@ -63,9 +63,9 @@ private:
   void ParListPrime(std::vector<PtrVarDecl>& par_list_inht, const ScopeId scope_inht);
   PtrVarDecl Par(const ScopeId scope_inht);
 
-  PtrExpr ArgList(std::vector<PtrExpr>& args_inht, const ScopeId scope_inht
+  PtrExpr ArgList(const std::string& name_inht, std::vector<PtrExpr>& args_inht, const ScopeId scope_inht
     , const Locus& locus_inht);
-  PtrExpr ArgListPrime(std::vector<PtrExpr>& args_inht, const ScopeId scope_inht
+  PtrExpr ArgListPrime(const std::string& name_inht, std::vector<PtrExpr>& args_inht, const ScopeId scope_inht
     , const Locus& locus_inht);
 
   //Helpers not associated to a rule
@@ -73,10 +73,10 @@ private:
   PtrVarDecl NameDecl(const Compiler::AST::Type& type_inht, const ScopeId scope_inht
                          , const Locus& locus_inht);
 
-  PtrExpr BuildFunctionCall(std::vector<PtrExpr>& args_inht, const ScopeId scope_inht
+  PtrExpr BuildFunctionCall(const std::string& name_inht, std::vector<PtrExpr>& args_inht, const ScopeId scope_inht
     , const Locus& locus_inht);
 
-  PtrExpr RecoveryArgList(std::vector<PtrExpr>& args_inht, const ScopeId scope_inht
+  PtrExpr RecoveryArgList(const std::string& name_inht, std::vector<PtrExpr>& args_inht, const ScopeId scope_inht
     , const Locus& locus_inht);
 
 
--- RecDescent/src/lib/LL1RecDescName.cpp
+++ RecDescent/src/lib/LL1RecDescName.cpp
@@ -15,7 +15,7 @@ PtrExpr ParserLL1RecDesc::Argm(const std::string& name_inht, const ScopeId scope
   //ARGM -> ( ARGL )  => (
   if(TryAndAccept(kToken::lpar)){
     std::vector<PtrExpr> args;
-    PtrExpr argm_synt = ArgList(args, scope_inht, locus_inht);
+    PtrExpr argm_synt = ArgList(name_inht, args, scope_inht, locus_inht);
     Accept(kToken::rpar, kErr35);
 
     return std::move(argm_synt);
@@ -32,44 +32,44 @@ PtrExpr ParserLL1RecDesc::Argm(const std::string& name_inht, const ScopeId scope
 // ex: void f(int a){..}, called with f() will be parsed as f
 //  nullptr <=> arguments were defective, function call is ignored
 //  !nullptr <=> pointer to expression with function call
-PtrExpr ParserLL1RecDesc::ArgList(std::vector<PtrExpr>& args_inht
+PtrExpr ParserLL1RecDesc::ArgList(const std::string& name_inht, std::vector<PtrExpr>& args_inht
   , const ScopeId scope_inht, const Locus& locus_inht){
   //ARGL -> {empty}  => {empty} )
   if(Check({kToken::rpar}))
-    return BuildFunctionCall(args_inht, scope_inht, locus_inht);
+    return BuildFunctionCall(name_inht, args_inht, scope_inht, locus_inht);
 
   //ARGL -> E ARGLP  => & * false ( {nam} {num}  true
   if(Check(set_expr_)){
     PtrExpr e = Exprs(scope_inht);
     if(e) args_inht.push_back(std::move(e));
-    return ArgListPrime(args_inht, scope_inht, locus_inht);
+    return ArgListPrime(name_inht, args_inht, scope_inht, locus_inht);
   }
 
   //Error recovery
-  return RecoveryArgList(args_inht, scope_inht, locus_inht);
+  return RecoveryArgList(name_inht, args_inht, scope_inht, locus_inht);
 }
 
 //  nullptr <=> arguments were defective, function call is ignored
 //  !nullptr <=> pointer to expression with function call
-PtrExpr ParserLL1RecDesc::ArgListPrime(std::vector<PtrExpr>& args_inht
+PtrExpr ParserLL1RecDesc::ArgListPrime(const std::string& name_inht, std::vector<PtrExpr>& args_inht
   , const ScopeId scope_inht, const Locus& locus_inht)
 {
   //ARGLP -> {empty}  => {empty} )
   if(Check({kToken::rpar}))
-    return BuildFunctionCall(args_inht, scope_inht, locus_inht);
+    return BuildFunctionCall(name_inht, args_inht, scope_inht, locus_inht);
 
   //ARGLP -> , E ARGLP  => ,
   if(TryAndAccept({kToken::comma}) and Check(set_expr_)){
     PtrExpr e = Exprs(scope_inht);
     if(e) args_inht.push_back(std::move(e));
-    return ArgListPrime(args_inht, scope_inht, locus_inht);
+    return ArgListPrime(name_inht, args_inht, scope_inht, locus_inht);
   }
 
   //Error recovery
-  return RecoveryArgList(args_inht, scope_inht, locus_inht);
+  return RecoveryArgList(name_inht, args_inht, scope_inht, locus_inht);
 }
 
-PtrExpr ParserLL1RecDesc::BuildFunctionCall(std::vector<PtrExpr>& args_inht
+PtrExpr ParserLL1RecDesc::BuildFunctionCall(const std::string& name_inht, std::vector<PtrExpr>& args_inht
   , const ScopeId scope_inht, const Locus& locus_inht){
   PtrExpr argm_synt(nullptr);
   if(args_inht.empty()) return std::move(argm_synt);
@@ -77,14 +77,34 @@ PtrExpr ParserLL1RecDesc::BuildFunctionCall(std::vector<PtrExpr>& args_inht
   //TODO: build missing arguments as default expresions
   // per type
 
-//   unit_.IsDeclValid(prev_token_string_value_
+  //Check that name's symbol does exists
+  if(not unit_.HasDecl(name_inht)) { Error(kErr37); return std::move(nullptr); }
+
+  const AST::FuncType& type_func =
+    dynamic_cast<const AST::FuncType&>(unit_.GetType(name_inht));
+
+  //Check if it is a function
+  if(not type_func.IsFunc()) { Error(kErr38); return std::move(nullptr); }
+
+  //Check number of arguments VS number of parameters
+  const size_t num_args = args_inht.size();
+  if( num_args < type_func.NumPars()) { Error(kErr39); return std::move(nullptr); }
+
+  if( num_args > type_func.NumPars()){
+    Error(kErr40);
+    size_t elements_to_remove = type_func.NumPars() - num_args;
+    for(size_t i = 0; i < elements_to_remove; ++i) args_inht.pop_back();
+    //continue to produce function call
+  }
+
+  for(const auto& arg : type_func){}
 }
 
-PtrExpr ParserLL1RecDesc::RecoveryArgList(std::vector<PtrExpr>& args_inht
+PtrExpr ParserLL1RecDesc::RecoveryArgList(const std::string& name_inht, std::vector<PtrExpr>& args_inht
   , const ScopeId scope_inht, const Locus& locus_inht){
   Error(kErr36);
   ConsumeTokensUntil(kToken::rpar);
-  return BuildFunctionCall(args_inht, scope_inht, locus_inht);
+  return BuildFunctionCall(name_inht, args_inht, scope_inht, locus_inht);
 }
 
 } //end namespace RecDescent
