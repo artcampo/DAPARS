e44f75cf60d96b71b756feffeaf13bfb70939530
dpl <tuchalia@gmail.com>
2017-04-30 14:23:36 +0200

    Activate test2, which uses Mov

--- DAVM/src/tests/test2.cpp
+++ DAVM/src/tests/test2.cpp
@@ -9,13 +9,13 @@
 int main(){
   using namespace VM;
   std::unique_ptr<ByteCode> bc(new ByteCode());
-  
+
   using namespace IRCodification;
   using namespace IRBuilder;
   using namespace IRDefinition;
   using namespace IRBuilderAPI;
   using namespace SubtypesComparison;
-  
+
   bc->stream = std::vector<uint32_t> {
     LoadI( IR_REG5, 42),
     Move ( IR_REG5, IR_REG1),
@@ -29,22 +29,19 @@ int main(){
     Comp ( IR_REG0, IR_REG1, IR_REG2, IR_LTE),
     Stop ()
   };
-  
-  
+
+
   if(!checkIRCodification()){
     std::cout << "IR sizes invalid\n";
     return 1;
   }
-  
-  VMUtils::printRaw(*bc);
-  VMUtils::print(*bc);
-//   std::cout << "test1 - started" << std::endl;
-//   for(auto it : bc->stream)
-//     std::cout << it <<", ";
-  
-//   std::unique_ptr<VirtualMachine> vm(new VirtualMachine(*bc) );
-//   vm->ExecProcess();
-//   vm->DumpExecutionContext();
-  
+
+  //VMUtils::printRaw(*bc);
+  //VMUtils::print(*bc);
+
+  std::unique_ptr<VirtualMachine> vm(new VirtualMachine(*bc) );
+  vm->ExecProcess();
+  vm->DumpExecutionContext();
+
   return 0;
 }
