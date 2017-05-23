8ce42a12f3c16885734d86109d320d45300965b5
ArturoCampos <arturocampos82@gmail.com>
2017-05-23 12:29:45 +0200

    Be: TestLang: if

--- RecDescent/execute_tests.sh
+++ RecDescent/execute_tests.sh
@@ -36,5 +36,5 @@ test_comp_davm lang_internal_test
 test_comp_davm lang_internal_test_false
 test_comp_davm error_stack_overflow
 test_comp_davm lang_while
-# test_comp_davm lang_if
+test_comp_davm lang_if
 test_comp_davm lang_args
